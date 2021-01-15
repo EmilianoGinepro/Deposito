0a4f2bfcd52eafe34d9b664a074eb84806c93c8a
sislac1986 <68289251+sislac1986@users.noreply.github.com>
2021-01-15 20:23:06 -0300

    login
    se sigue mejorando el login

--- Deposito/DepositoVisual/Ventana_Login.Designer.cs
+++ Deposito/DepositoVisual/Ventana_Login.Designer.cs
@@ -31,29 +31,60 @@ namespace DepositoVisual
         {
             this.txtUsuario = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtClave = new System.Windows.Forms.TextBox();
+            this.btnIngresar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtUsuario
             // 
-            this.txtUsuario.Location = new System.Drawing.Point(282, 82);
+            this.txtUsuario.Location = new System.Drawing.Point(82, 81);
             this.txtUsuario.Name = "txtUsuario";
-            this.txtUsuario.Size = new System.Drawing.Size(100, 23);
+            this.txtUsuario.Size = new System.Drawing.Size(123, 23);
             this.txtUsuario.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(282, 45);
+            this.label1.Location = new System.Drawing.Point(82, 43);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(47, 15);
             this.label1.TabIndex = 1;
             this.label1.Text = "Usuario";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(82, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Clave";
+            // 
+            // txtClave
+            // 
+            this.txtClave.Location = new System.Drawing.Point(82, 173);
+            this.txtClave.Name = "txtClave";
+            this.txtClave.Size = new System.Drawing.Size(123, 23);
+            this.txtClave.TabIndex = 3;
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(110, 224);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(75, 23);
+            this.btnIngresar.TabIndex = 4;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            // 
             // Ventana_Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(260, 280);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.txtClave);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtUsuario);
             this.Name = "Ventana_Login";
@@ -67,5 +98,8 @@ namespace DepositoVisual
 
         private System.Windows.Forms.TextBox txtUsuario;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtClave;
+        private System.Windows.Forms.Button btnIngresar;
     }
 }
